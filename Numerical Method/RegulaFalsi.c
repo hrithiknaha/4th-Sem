9c62ada9fc94a2cd9695f45e8875dd012dba5c80
Hrithik Naha <naha.hrithkk@gmail.com>
2018-02-12 21:22:51 +0530

    add regulafalsi.c

---
+++ Numerical Method/RegulaFalsi.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+#define f(x)((x*x*x)-(5*x)-7)
+#define acc 0.001
+int main()
+{
+	float a,b,x;
+	float c=0;
+		printf(".................\n");
+		do
+		{
+				if(f(c)>0)
+				 	b=c;
+				else
+					a=c;
+				c=c+0.1;
+				printf("\n%lf\t%lf",c,f(c));
+		}while(f(a)*f(b)>0);
+		printf("\nInterval is [%lf,%lf]\n",a,b);
+	printf("\nThe Value of x :");
+	do
+		{
+			x=a-((b-a)*(f(a)))/(f(b)-f(a));
+			if(f(a)*f(b)<0)
+				b=x;
+			else
+				a=x;
+			printf("%f \n",x);
+		}while(fabs(b-a)>acc);
+		printf("\nThe Root is %f",x);
+	return 1;
+}
+			
